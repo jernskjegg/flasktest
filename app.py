00c8117a80720f421b26241145418c1f2b2b74e5
jernskjegg <64691940+jernskjegg@users.noreply.github.com>
2021-02-15 15:24:22 +0100

    testing routing

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ def form_get():
     """
 @app.route("/form", methods=["POST"])
 def form_post():
-    user_input = request.form["user_input"]
-    return f"<h1> The user input was: { user_input }</h1>"
+    user_potato = request.form["user_input"]
+    return f"<h1> The user input was: { user_potato }</h1>"
 
 @app.route("/accept_json", methods= ["POST"])
 def accept_json():
